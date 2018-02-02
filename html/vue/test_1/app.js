dc027b2752c9845ef4c73db81018ebd88dc9db07
huguanghui <522146829@qq.com>
2018-02-02 19:08:49 +0800

    refresh vue_test

--- html/vue/test_1/app.js
+++ html/vue/test_1/app.js
@@ -1,7 +1,15 @@
+Vue.component('todo-item', {
+	props: ['todo'],
+	template:'<li>{{ todo.text }}</li>'
+})
+
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  },
+  created: function () {
+  	console.log(this.message);
   }
 })
 
@@ -20,6 +28,9 @@ var app4 = new Vue({
 			{text: '运动'},
 			{text: '睡觉'}
 		]
+	},
+	created: function () {
+		console.log(this.todos);
 	}
 })
 
@@ -38,6 +49,25 @@ var app5 = new Vue({
 var app6 = new Vue({
 	el: '#app-6',
 	data: {
-		message: 'Hello'
+		message: 'Hello',
+		items:[
+			{ id: 0, text: 'aa'},
+			{ id: 1, text: 'bb'}
+		]
+	}
+})
+
+var app7 = new Vue({
+	el: '#app-7',
+	data: {
+		a: 2,
+		groceryList: [
+		  { id: 0, text: '蔬菜'},
+		  { id: 1, text: '奶酪'},
+		  { id: 2, text: '肉'}
+		]
+	},
+	created: function () {
+		console.log('a is: ' + this.a)
 	}
 })
