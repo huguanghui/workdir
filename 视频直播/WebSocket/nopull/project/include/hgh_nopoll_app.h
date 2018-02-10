3c7fddbd4c7268ae72dcf259b7380c147220bda4
huguanghui <522146829@qq.com>
2018-02-10 18:17:25 +0800

    refresh

---
+++ 视频直播/WebSocket/nopull/project/include/hgh_nopoll_app.h
@@ -0,0 +1,10 @@
+#ifndef __HGH_NOPOLL_APP_H__
+#define __HGH_NOPOLL_APP_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "nopoll.h"
+
+#endif
---
+++ 视频直播/WebSocket/nopull/project/src/hgh_nopoll_main.c
@@ -0,0 +1,10 @@
+#include "hgh_nopoll_app.h"
+
+int main(int argc, char *argv[])
+{
+	int iRet;
+
+	printf("Hello World!\n");
+
+	return iRet;
+}
