315599d146b9aefcc04ee700a1c4e46f283f47d4
hugh <2936658515@qq.com>
2018-02-11 00:24:16 +0800

    refresh

--- 视频直播/WebSocket/nopull/project/include/hgh_nopoll_app.h
+++ 视频直播/WebSocket/nopull/project/include/hgh_nopoll_app.h
@@ -1,10 +1,9 @@
 #ifndef __HGH_NOPOLL_APP_H__
 #define __HGH_NOPOLL_APP_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include "nopoll.h"
+#include <hgh_nopoll_common.h>
+#include <nopoll.h>
+
+#include <signal.h>
 
 #endif
--- 视频直播/WebSocket/nopull/project/src/hgh_nopoll_main.c
+++ 视频直播/WebSocket/nopull/project/src/hgh_nopoll_main.c
@@ -1,10 +1,30 @@
 #include "hgh_nopoll_app.h"
 
-int main(int argc, char *argv[])
+noPollCtx *ctx = NULL;
+
+int main(int argc, char **argv)
 {
-	int iRet;
+	noPollConn *listener;
+	int iterator;
 
-	printf("Hello World!\n");
+	printf("INFO: install default threading functions to check noPoll locking code..\n");
+	nopoll_thread_handlers(__nopoll_regtest_mutex_create, 
+		__nopoll_regtest_mutex_destroy,
+		__nopoll_regtest_mutex_lock,
+		__nopoll_regtest_mutex_unlock);
 
-	return iRet;
+	/* create the context */
+	ctx = nopoll_ctx_new();
+
+	iterator = 1;
+	while (iterator < argc)
+	{
+		
+	}
+
+	nopoll_ctx_unref(ctx);
+
+	nopoll_cleanup_library();
+
+	return 0;
 }
